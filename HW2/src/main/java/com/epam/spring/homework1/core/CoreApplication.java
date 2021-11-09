e49ea5c4f35846e05cb1fd3efdb8a5911eef4a91
Anastasiia <adermahnastya@gmail.com>
2021-11-09 21:57:14 +0200

    second homework

--- HW2/src/main/java/com/epam/spring/homework1/core/CoreApplication.java
+++ HW2/src/main/java/com/epam/spring/homework1/core/CoreApplication.java
@@ -2,17 +2,13 @@ package com.epam.spring.homework1.core;
 
 import com.epam.spring.homework1.core.config.BeansConfig;
 import com.epam.spring.homework1.core.pet.Pet;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-@SpringBootApplication
+
 public class CoreApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CoreApplication.class, args);
 		ApplicationContext context = new AnnotationConfigApplicationContext(BeansConfig.class);
 		context.getBean(Pet.class).printPets();
 	}
