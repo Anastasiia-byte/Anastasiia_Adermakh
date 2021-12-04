de310917ad83dcbd75141c3c1ef2b3787cdc9580
Anastasiia-byte <72467915+Anastasiia-byte@users.noreply.github.com>
2021-12-04 18:27:55 +0200

    Delete HW6 directory

--- HW6/src/main/java/com/epam/springboot/homework1/core/exception/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.epam.springboot.homework1.core.exception;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message){
-        super(message);
-    }
-}
